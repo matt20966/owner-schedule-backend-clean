0ae572ada54ad012b09f9231bf7f4072cc47860b
matt20966 <mwhalley2003@gmail.com>
2025-09-14 11:43:02 +0100

    latest

--- owner_schedule_backend/settings.py
+++ owner_schedule_backend/settings.py
@@ -95,15 +95,12 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = "static/"
-
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Folder where collectstatic will put all static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'owner_schedule_backend', 'frontend', 'dist')]
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'owner_schedule_backend', 'frontend', 'dist')]
